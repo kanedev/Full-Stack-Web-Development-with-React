7a9e6099b2e4e958a09dfffa1594c8344392018f
kanedev <kanedev@live.fr>
2020-06-11 11:57:10 +0200

    Assignement 1 : completed

--- Front-End Web Development with React/confusion/src/App.js
+++ Front-End Web Development with React/confusion/src/App.js
@@ -1,6 +1,5 @@
 import React, {Component} from 'react';
 import { DISHES } from './shared/dishes';
-import logo from './logo.svg';
 import { Navbar, NavbarBrand } from 'reactstrap';
 import  Menu  from "./components/MenuComponent";
 import './App.css';
---
+++ Front-End Web Development with React/confusion/src/components/DishdetailComponent.js
@@ -0,0 +1,86 @@
+import React, { Component } from 'react';
+import { Card, CardImg, CardText, CardBody,
+    CardTitle } from 'reactstrap'; 
+
+
+class DishDetail extends Component {
+    constructor(props) {
+        super(props);
+        this.state = {  };
+    }
+
+    renderDish(dish) {
+        if (dish != null)
+            return(
+                <Card>
+                <CardImg top src={dish.image} alt={dish.name} />
+                <CardBody>
+                  <CardTitle>{dish.name}</CardTitle>
+                  <CardText>{dish.description}</CardText>
+                </CardBody>
+                </Card>
+            );
+        else
+            return(
+                <div></div>
+            );
+    }
+
+
+
+
+    renderComments(dish){
+        
+        if (dish != null)
+        return (
+           
+           <div  className="">
+                <h4> Comments</h4>
+                   <ul className=" list-unstyled">
+              {
+              dish.comments.map((comment) => {
+                   return (
+                      <li  key={comment.id} className=" ">
+                        <p>{comment.comment}</p>
+                        <p>-- {comment.author}, &nbsp;
+                        {new Intl.DateTimeFormat('en-US', {
+                            year: 'numeric',
+                            month: 'long',
+                            day: '2-digit'
+                        }).format(new Date(comment.date))}
+                    </p>
+                    </li>
+                  )
+               })
+              }  
+            </ul>
+            </div>
+           
+          )
+        else
+            return(
+                <div></div>
+            );  
+
+    }
+    render() {
+
+        return (
+            
+            <div className="row">
+            <div  className="col-12 col-md-5 m-1"> 
+            {this.renderDish(this.props.dish)}
+            </div>
+            <div  className="col-12 col-md-5 m-1"> 
+            {this.renderComments(this.props.dish)}
+            </div>
+            </div>
+
+
+        );
+    }
+}
+
+export default DishDetail;
+
+
