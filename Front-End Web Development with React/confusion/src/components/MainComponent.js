a0a209e9da879e2df47bce8be1e4ca0053222a9e
kanedev <kanedev@live.fr>
2020-06-14 20:59:52 +0200

    Single Page Application part 1

--- Front-End Web Development with React/confusion/src/components/MainComponent.js
+++ Front-End Web Development with React/confusion/src/components/MainComponent.js
@@ -3,7 +3,11 @@ import Header from './HeaderComponent';
 import Footer from './FooterComponent';
 import  Menu  from "./MenuComponent";
 import Home from './HomeComponent';
+import Contact from './ContactComponent';
 import { DISHES } from '../shared/dishes';
+import { COMMENTS } from '../shared/comments';
+import { PROMOTIONS } from '../shared/promotions';
+import { LEADERS } from '../shared/leaders';
 
 import  DishDetail  from './DishdetailComponent';
 
@@ -16,7 +20,10 @@ class Main extends Component {
     super(props);
     this.state = {
       dishes: DISHES,
-      selectDish:null
+      selectDish:null,
+      comments: COMMENTS,
+      promotions: PROMOTIONS,
+      leaders: LEADERS
     };
   }
 
@@ -27,11 +34,14 @@ class Main extends Component {
   render() {
 
     const HomePage = () => {
-        return(
-            <Home 
-            />
-        );
-      }
+      return(
+          <Home 
+              dish={this.state.dishes.filter((dish) => dish.featured)[0]}
+              promotion={this.state.promotions.filter((promo) => promo.featured)[0]}
+              leader={this.state.leaders.filter((leader) => leader.featured)[0]}
+          />
+      );
+    }
   
 
   return (
@@ -42,6 +52,7 @@ class Main extends Component {
             <Switch>
                 <Route path='/home' component={HomePage} />
                 <Route exact path='/menu' component={() => <Menu dishes={this.state.dishes} />} />
+                <Route exact path='/contactus' component={Contact} />} />
                 <Redirect to="/home" />
             </Switch>
 
