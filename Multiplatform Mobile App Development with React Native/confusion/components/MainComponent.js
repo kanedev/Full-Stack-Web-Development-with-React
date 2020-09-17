bf60abb58948ac1b2bb15eeca1d305aa23bc7f23
kanedev <kanedev@live.fr>
2020-09-17 23:18:23 +0200

    React Native Navigation Part 2

--- Multiplatform Mobile App Development with React Native/confusion/components/MainComponent.js
+++ Multiplatform Mobile App Development with React Native/confusion/components/MainComponent.js
@@ -3,7 +3,9 @@ import Menu from './MenuComponent';
 import Dishdetail from './DishdetailComponent';
 import { DISHES } from '../shared/dishes';
 import { View, Platform } from 'react-native';
-import { createStackNavigator } from 'react-navigation';
+import Home from './HomeComponent';
+import { createStackNavigator, createDrawerNavigator } from 'react-navigation';
+import { Icon } from 'react-native-elements';
 
 
 const MenuNavigator = createStackNavigator({
@@ -24,6 +26,38 @@ const MenuNavigator = createStackNavigator({
 }
 );
 
+const HomeNavigator = createStackNavigator({
+  Home: { screen: Home }
+}, {
+  navigationOptions: ({ navigation }) => ({
+    headerStyle: {
+        backgroundColor: "#512DA8"
+    },
+    headerTitleStyle: {
+        color: "#fff"            
+    },
+    headerTintColor: "#fff"  
+  })
+});
+
+const MainNavigator = createDrawerNavigator({
+  Home: 
+    { screen: HomeNavigator,
+      navigationOptions: {
+        title: 'Home',
+        drawerLabel: 'Home'
+      }
+    },
+  Menu: 
+    { screen: MenuNavigator,
+      navigationOptions: {
+        title: 'Menu',
+        drawerLabel: 'Menu'
+      }, 
+    }
+}, {
+drawerBackgroundColor: '#D1C4E9'
+});
 
 
 class Main extends Component {
@@ -34,7 +68,7 @@ class Main extends Component {
       // Uncomment this  line in prod mode ;-)
       // <View style={{flex:1, paddingTop: Platform.OS === 'ios' ? 0 : Expo.Constants.statusBarHeight }}>
       <View style={{flex:1}}>
-      <MenuNavigator />
+      <MainNavigator />
       </View>
     );
   }
