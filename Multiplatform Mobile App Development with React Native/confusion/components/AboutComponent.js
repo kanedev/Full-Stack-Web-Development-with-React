e39f0794c6a510b3d2117a37d32c1c453a2be313
kanedev <kanedev@live.fr>
2020-09-19 20:49:28 +0200

    Activity Indicator

--- Multiplatform Mobile App Development with React Native/confusion/components/AboutComponent.js
+++ Multiplatform Mobile App Development with React Native/confusion/components/AboutComponent.js
@@ -2,8 +2,10 @@ import React, { Component } from 'react';
 import { Card } from 'react-native-elements';
 import { Text,View,ScrollView, FlatList } from 'react-native';
 import { ListItem } from 'react-native-elements';
+import { LEADERS } from '../shared/leaders';
 import { connect } from 'react-redux';
 import { baseUrl } from '../shared/baseUrl';
+import { Loading } from './LoadingComponent';
 
 const mapStateToProps = state => {
     return {
@@ -14,13 +16,13 @@ const mapStateToProps = state => {
 
 function  History () {
     return (
-           <View>
+        <Card title="Our History">  
                    <Text style = {{margin: 10}}>     
 Started in 2010, Ristorante con Fusion quickly established itself as a culinary icon par excellence in Hong Kong. With its unique brand of world fusion cuisine that can be found nowhere else, it enjoys patronage from the A-list clientele in Hong Kong.  Featuring four of the best three-star Michelin chefs in the world, you never know what will arrive on your plate the next time you visit us.
     </Text>
     <Text style = {{margin: 10}}>The restaurant traces its humble beginnings to The Frying Pan, a successful chain started by our CEO, Mr. Peter Pan, that featured for the first time the world's best cuisines in a pan.</Text>
  
-           </View>
+           </Card>
 
  )
 }
@@ -28,6 +30,12 @@ Started in 2010, Ristorante con Fusion quickly established itself as a culinary 
 
 class About extends Component {
 
+    constructor(props) {
+        super(props);
+        this.state = {
+            leaders: LEADERS
+        };
+    }
 
     static navigationOptions = {
         title: 'About us',
@@ -45,38 +53,50 @@ class About extends Component {
         title={item.name}
         subtitle={item.description}
         hideChevron={true}
-        leftAvatar={{source: {uri: baseUrl + item.image}}}
+        leftAvatar={{ source: require('./images/alberto.png')}}
       />
     );
 };
 
+if (this.props.leaders.isLoading) {
+    return(
+        <ScrollView>
+            <History />
+            <Card
+                title='Corporate Leadership'>
+                <Loading />
+            </Card>
+        </ScrollView>
+    );
+}
+else if (this.props.leaders.errMess) {
+    return(
+        <ScrollView>
+            <History />
+            <Card
+                title='Corporate Leadership'>
+                <Text>{this.props.leaders.errMess}</Text>
+            </Card>
+        </ScrollView>
+    );
+}
+else {
+    return(
+        <ScrollView>
+            <History />
+            <Card
+                title='Corporate Leadership'>
+            <FlatList 
+                data={this.props.leaders.leaders}
+                renderItem={renderLeadersItem}
+                keyExtractor={item => item.id.toString()}
+                />
+            </Card>
+        </ScrollView>
+    );
+}
+}
 
-
-
-
-
-
-
-
-        return(
-
-<ScrollView>
-<Card title="Our History">    
-<History/>
-</Card>
-<Card title="Corporate Leadership">
-<FlatList 
- data={this.props.leaders.leaders}
-renderItem={renderLeadersItem}
-keyExtractor={item => item.id.toString()}
-/>   
-</Card>
-
-</ScrollView>
-
-
-        );
-    }
 }
 
 export default connect(mapStateToProps)(About);
--- Multiplatform Mobile App Development with React Native/confusion/components/MenuComponent.js
+++ Multiplatform Mobile App Development with React Native/confusion/components/MenuComponent.js
@@ -5,6 +5,8 @@ import { Tile } from 'react-native-elements';
 
 import { connect } from 'react-redux';
 import { baseUrl } from '../shared/baseUrl';
+import { Loading } from './LoadingComponent';
+
 
 const mapStateToProps = state => {
     return {
@@ -40,13 +42,27 @@ class Menu extends Component  {
 
         const { navigate } = this.props.navigation;
 
-        return (
-                  <FlatList 
+        if (this.props.dishes.isLoading) {
+            return(
+                <Loading />
+            );
+        }
+        else if (this.props.dishes.errMess) {
+            return(
+                <View>            
+                    <Text>{props.dishes.errMess}</Text>
+                </View>            
+            );
+        }
+        else {
+            return (
+                <FlatList 
                     data={this.props.dishes.dishes}
                     renderItem={renderMenuItem}
                     keyExtractor={item => item.id.toString()}
                     />
-    );
+            );
+        };
 }
 }
 
