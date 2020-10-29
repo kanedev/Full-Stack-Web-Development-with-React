00902e7f750505ddd6524636ca67f7477d86a9d3
kanedev <kanedev@live.fr>
2020-10-29 19:54:39 +0100

    Assignment 1: Node Modules, Express and REST API

--- Server-side Development with NodeJS, Express and MongoDB/node-express/index.js
+++ Server-side Development with NodeJS, Express and MongoDB/node-express/index.js
@@ -2,8 +2,10 @@ const express = require('express'),
       http = require('http');
 
 const morgan = require('morgan');
-const bodyParser = require('body-parser');
+//const bodyParser = require('body-parser');
 const dishRouter = require('./routes/dishRouter');
+const leaderRouter = require('./routes/leaderRouter');
+const promoRouter = require('./routes/promoRouter');
 
 
 const hostname = 'localhost';
@@ -12,6 +14,8 @@ const port = 3000;
 const app = express();
 app.use(morgan('dev'));
 app.use('/dishes', dishRouter);
+app.use('/leaders', leaderRouter);
+app.use('/promos', promoRouter);
 app.use(express.static(__dirname + '/public'));
 
 app.use((req, res, next) => {
