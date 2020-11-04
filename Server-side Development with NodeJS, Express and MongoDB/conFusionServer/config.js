fd64e2a40d2dbf3371d52567783e1835ece0805d
kanedev <kanedev@live.fr>
2020-11-04 14:04:50 +0100

    Passport Facebook

--- Server-side Development with NodeJS, Express and MongoDB/conFusionServer/config.js
+++ Server-side Development with NodeJS, Express and MongoDB/conFusionServer/config.js
@@ -1,4 +1,8 @@
 module.exports = {
     'secretKey': '12345-67890-09876-54321',
-    'mongoUrl' : 'mongodb://localhost:27018/conFusion'
+    'mongoUrl' : 'mongodb://localhost:27018/conFusion',
+    'facebook': {
+        clientId: '631701180833184',
+        clientSecret: '0a9d5e7e41284acac930190f51996624'
+                 }
 }
